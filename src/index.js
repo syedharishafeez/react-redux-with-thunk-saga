1e350f778f5a67530f90d7fa261a68488c36bcd5
syedharishafeez <syed_haris98@yahoo.com>
2021-03-23 20:07:57 +0500

    Implementing Thunk middleware is

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,14 @@
 export const AGE_UP = () => {
+  // this function can only be return if and only if thunk middleware is applied otherwise react will give following error
+  // "Error: Actions must be plain objects. Use custom middleware for async actions."
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(AGE_UP_ASYNC());
+    }, 1000);
+  };
+};
+
+export const AGE_UP_ASYNC = () => {
   return { type: "AGE_UP", value: 1 };
 };
 
